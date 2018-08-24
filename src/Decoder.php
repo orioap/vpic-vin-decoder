9d9873afaeea8243e07236a04f2890ca654f5eb9
Ryan Andrews <r_andrews1981@hotmail.com>
2018-08-24 07:55:39 -0400

    Renamed method from getModels to models Added variables function

--- src/Decoder.php
+++ src/Decoder.php
@@ -9,7 +9,7 @@ class Decoder {
 		$response 	= $client->request('GET', 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/'.$vin.'?format=json');
 
 		$result		= json_decode($response->getBody());
-		//echo get_class($result);exit;
+
 		if(isset($result->Results)){
 			foreach($result->Results AS $key => $data){
 				if($data->Value){
@@ -26,19 +26,41 @@ class Decoder {
 		return $res;
 	}
 
-	public static function getModels($make){
+	public static function models($make){
 		$client     = new Client();
 		$res = collect();
 		$response 	= $client->request('GET', 'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/'.$make.'?format=json');
 
 		$result		= json_decode($response->getBody());
-		//echo get_class($result);exit;
+
 		if(isset($result->Results)){
 			foreach($result->Results AS $key => $data){
 				if($data->Model_ID){
 					$res->push((object)[
 						'id' => $data->Model_ID,
 						'name' => $data->Model_Name,
+					]);
+				}
+			}
+		}
+		return $res;
+	}
+
+	public static function variables(){
+		$client     = new Client();
+		$res = collect();
+		$response 	= $client->request('GET', 'https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist?format=json');
+
+		$result		= json_decode($response->getBody());
+
+		if(isset($result->Results)){
+			foreach($result->Results AS $key => $data){
+				if($data->ID){
+					$res->push((object)[
+						'id' => $data->ID,
+						'name' => $data->Name,
+						'desc' => $data->Description,
+						'type' => $data->DataType,
 					]);
 				}
 			}
